0229edaa9bf3efd583834a81d553db76b7745aa0
Breyner Rojas <brojas22@outlook.com>
2022-04-11 10:03:12 -0500

    Improve FindRunningMedia performance by adding PriorityQueues

--- Java/src/main/java/com/github/tavet/challenges/general/FindRunningMedia.java
+++ Java/src/main/java/com/github/tavet/challenges/general/FindRunningMedia.java
@@ -3,7 +3,7 @@ package com.github.tavet.challenges.general;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.PriorityQueue;
 
 /**
  * https://www.hackerrank.com/challenges/find-the-running-median/problem
@@ -13,22 +13,42 @@ public class FindRunningMedia {
         if (a.isEmpty() || a.size() == 1) {
             return Collections.emptyList();
         }
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
         List<Double> answer = new ArrayList<>();
-        List<Integer> operations = new ArrayList<>();
         int length = a.get(0);
 
         for (int i = 1; i < length + 1; i++) {
-            operations.add(a.get(i));
-            Collections.sort(operations);
+            int number = a.get(i);
+            double operation;
 
-            double operation = 0;
-            if (operations.size() % 2 == 0) {
-                operation = ((double) operations.get(operations.size() / 2) +
-                        (double) operations.get((operations.size() / 2) - 1))
-                        / 2;
-            } else {
-                operation = (double) operations.get(operations.size() / 2);
+            if(maxHeap.isEmpty()) {
+                maxHeap.add(a.get(i));
+            } else if(maxHeap.size() == minHeap.size()) {
+                if(number < minHeap.peek()) {
+                    maxHeap.add(number);
+                } else {
+                    minHeap.add(number);
+                    maxHeap.add(minHeap.poll());
+                }
+            } else if (maxHeap.size() > minHeap.size()) {
+                if(number > maxHeap.peek()) {
+                    minHeap.add(number);
+                } else {
+                    maxHeap.add(number);
+                    minHeap.add(maxHeap.poll());
+                }
             }
+
+            if(maxHeap.isEmpty()) {
+                return Collections.singletonList(0.0);
+            } else if(maxHeap.size() == minHeap.size()) {
+                operation = (maxHeap.peek() + minHeap.peek()) / 2.0;
+            } else {
+                operation = maxHeap.peek();
+            }
+
             answer.add(operation);
         }
 
